55e86b3083ec72e6e99db617fc1efbc5e9864a40
DanhBito <0974619741danh@gmail.com>
2021-04-26 11:28:22 +0700

    404

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return redirect(route('home'));
+            }
+        }
         return parent::render($request, $exception);
     }
 }
